f6e197497e3a9f0edcedf07b7a2a1bd7cefb3475
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-04-28 18:08:56 +0200

    Implement user registration

--- models/user.js
+++ models/user.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, process.env.HARMONEY_SECRET_KEY);
     return token;
 };
 
