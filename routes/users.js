7d72db9bf10aeb8d7c252779f939569d761ce2e9
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-06-03 18:53:59 +0200

    Add friends field to user creation

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
 
-  user = new User(_.pick(req.body, ['name', 'email', 'password', 'firstName', 'lastName']));
+  user = new User(_.pick(req.body, ['name', 'email', 'password', 'firstName', 'lastName', 'friends']));
   user.isAdmin = false;
   user.isVerified = false;
   user.profileImage = "";
