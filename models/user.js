5b086304f64c5b7e27f75fce9ab47239b30784a2
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-06-09 19:40:20 +0200

    Add confirmed filed to user model

--- models/user.js
+++ models/user.js
@@ -53,9 +53,18 @@ const userSchema = new mongoose.Schema({
     },
 
     friends: {
-        type: [mongoose.Schema.Types.ObjectID],
-        Ref: this,
-        default: []
+        type: mongoose.Schema.Types.Array,
+        friend: {
+            type: mongoose.Schema.Types.Object,
+            id: {
+                type: mongoose.Schema.Types.ObjectID,
+                Ref: this,
+            },
+            confirmed: {
+                type: Boolean,
+                default: false
+            }
+        }
     }
 }, {timestamps: true});
 
