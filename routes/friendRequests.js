49393e5c80f9a81400c68e1cb1e9b574705f38e0
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-06-11 14:35:24 +0200

    Fix update route

--- routes/friendRequests.js
+++ routes/friendRequests.js
@@ -8,13 +8,34 @@ const {User} = require('../models/user');
 router.post('/add', auth, cors(), async (req, res) => {
     const currentUser = await User.findById(req.user._id);
     const requestedUser = await User.findById(req.body.friendId.toString());
-    const alreadyFriends = await FriendRequest.findOne({from: currentUser._id}, {to: requestedUser._id});
-    if (alreadyFriends) return res.status(400).send("You've already sent a request");
+    // const alreadyFriends = await FriendRequest.findOne({from: currentUser._id}, {to: requestedUser._id});
+    // if (alreadyFriends) return res.status(400).send("You've already sent a request");
     const request = await new FriendRequest();
     request.from = currentUser._id;
     request.to = requestedUser._id;
     await request.save();
-    await addUnconfirmedFriend(req, res, requestedUser);
+    // await addUnconfirmedFriend(req, res);
+    if (currentUser.friends.forEach((friend => friend.id === requestedUser))) return res.status(400).send("You are already friends");
+    const result = await User.findOneAndUpdate({_id: currentUser._id}, {
+        $push: {
+            friends: {
+                id: requestedUser,
+                confirmed: false
+            }
+        }
+    });
+    if (!result) return res.status(400).send('No such document');
+    const result2 = await User.findOneAndUpdate({_id: requestedUser._id}, {
+        $push: {
+            friends: {
+                id: currentUser,
+                confirmed: false
+            }
+        }
+    });
+    if (!result2) return res.status(400).send('No such document');
+    // res.status(200).send('Friend added successfully');
+
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
     res.status(200).send(request);
 
@@ -49,10 +70,8 @@ router.get('/getFriends', auth, async (req, res) => {
     const friendsObjects = currentUser.friends;
     const friends = [];
     for (const friendObject of friendsObjects) {
-        for (const friendId of friendObject) {
-            const friend = friendId;
+            const friend = friendObject;
             friends.push(friend);
-        }
     }
     res.status(200).send(friends);
 });
@@ -65,56 +84,17 @@ router.delete('/:id', auth, async (req, res) => {
     res.status(200).send("Document deleted");
 });
 
-async function addUnconfirmedFriend(req, res, requestedUser) {
-    const currentUser = await User.findById(req.user._id);
-    // const friendToAdd = await User.findById(req.params.id);
-    if (currentUser.friends.forEach(friends => friends.forEach(friend => friend.friend.id === requestedUser))) return res.status(400).send("You are already friends");
-    const result = await User.update({_id: currentUser._id}, {
-        $push: {
-            friends: {
-                friend: {
-                    id: requestedUser,
-                    confirmed: false
-                }
-            }
-        }
-    });
-    if (!result) return res.status(400).send('No such document');
-    const result2 = await User.update({_id: requestedUser._id}, {
-        $push: {
-            friends: {
-                friend: {
-                    id: currentUser,
-                    confirmed: false
-                }
-            }
-        }
-    });
-    if (!result2) return res.status(400).send('No such document');
-    res.status(200).send('Friend added successfully');
-}
-
 router.patch('/:id', auth, async (req, res) => {
     const currentUser = await User.findById(req.user._id);
     const friendToAdd = await User.findById(req.params.id);
-    if (currentUser.friends.forEach(friends => friends.forEach(friend => friend.friend.id === friendToAdd))) return res.status(400).send("You are already friends");
-    const result = await User.update({_id: currentUser._id}, {
-        friends: {
-            friend: {
-                id: friendToAdd,
-                confirmed: true
-            }
-        }
-    }, printError);
+    if (currentUser.friends.forEach((friend => friend.id === friendToAdd))) return res.status(400).send("You are already friends");
+    const result = await User.findOneAndUpdate({_id: friendToAdd._id}, {
+        $set:{'friends.$[friend].confirmed':  true}
+    },{new:true,arrayFilters: [{'friend.id._id': currentUser._id}  ]}, printError);
     if (!result) return res.status(400).send('No such document');
-    const result2 = await User.update({_id: friendToAdd._id}, {
-        friends: {
-            friend: {
-                id: currentUser,
-                confirmed: true
-            }
-        }
-    });
+    const result2 = await User.findOneAndUpdate({_id: currentUser._id}, {
+        $set:{'friends.$[friend].confirmed':  true}
+    },{arrayFilters: [{'friend.id._id': friendToAdd._id}  ], new: true}, printError);
     if (!result2) return res.status(400).send('No such document');
     res.status(200).send('Friend added successfully');
 
